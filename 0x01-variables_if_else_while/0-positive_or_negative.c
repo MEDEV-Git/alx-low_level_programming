1c716bf5e0db6bdaf787c56d86586b0174db9d3f
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-09 03:24:23 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/*
+* main function
+*
+* Description : negative or positive
+*/
 int main(void)
 {
 	int n;
