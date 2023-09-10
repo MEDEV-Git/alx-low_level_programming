34fc1f3a8fb164491085fc370ad4d16c01219942
Mohammed Goubi <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-10 15:49:34 -0700

    Updae 6-size.c Betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,27 +10,26 @@
 
 int main(void)
 {
-    
-    int ch = sizeof(char);
+	int ch = sizeof(char);
 
-    printf("Size of a char: %d byte(s)\n", ch);
+	printf("Size of a char: %d byte(s)\n", ch);
 
-    int n = sizeof(int);
+	int n = sizeof(int);
 
-    printf("Size of an int: %d byte(s)\n", n);
+	printf("Size of an int: %d byte(s)\n", n);
 
-    long lint = sizeof(long);
+	long lint = sizeof(long);
 
-    printf("Size of a long int: %ld byte(s)\n", lint);
+	printf("Size of a long int: %ld byte(s)\n", lint);
 
-    long long llint = sizeof(long long);
+	long long llint = sizeof(long long);
 
-    printf("Size of a long long int: %lld byte(s)\n", llint);
+	printf("Size of a long long int: %lld byte(s)\n", llint);
 
-    float f = sizeof(float);
+	float f = sizeof(float);
 
-    printf("Size of a float: %g byte(s)\n", f);
+	printf("Size of a float: %g byte(s)\n", f);
 
-    return (0);
-    
+	return (0);
+
 }
