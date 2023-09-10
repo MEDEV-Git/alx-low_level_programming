7f35b281f19fd9d4272d9cb301fe5f482e1e7cef
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-10 23:44:49 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
+
 /**
- * main - entry point
+ * main - Entry point
  *
- * Description: print the sizeof
+ * Description: Print the sizes using sizeof
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int ch = sizeof(char);
-	
-	printf("Size of a char: %d byte(s)\n", ch);
-	
-	int n = sizeof(int);
-	
-	printf("Size of an int: %d byte(s)\n", n);
-	
-	int lint = sizeof(long);
-	
-	printf("Size of a long int: %ld byte(s)\n", lint);
-	
-	long llint = sizeof(long long);
-	
-	printf("Size of a long long int: %lld byte(s)\n", llint);
-	
-	float f = sizeof(float);
-	
-	printf("Size of a float: %g byte(s)\n", f);
-	
-	return (0);
+    int ch = sizeof(char);
 
+    printf("Size of a char: %d byte(s)\n", ch);
+
+    int n = sizeof(int);
+
+    printf("Size of an int: %d byte(s)\n", n);
+
+    long lint = sizeof(long);
+
+    printf("Size of a long int: %ld byte(s)\n", lint);
+
+    long long llint = sizeof(long long);
+
+    printf("Size of a long long int: %lld byte(s)\n", llint);
+
+    float f = sizeof(float);
+
+    printf("Size of a float: %g byte(s)\n", f);
+
+    return (0);
 }
