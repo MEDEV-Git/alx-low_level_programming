6679f147d8ba7b661ed66a042f5cb7dd8662e731
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-11 00:16:56 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,14 +11,12 @@
 int main(void)
 {
     char letter;
-
+    
     for (letter = 'a'; letter <= 'z'; letter++)
     {
         putchar(letter);
     }
-
     putchar('\n');
     return (0);
 
 }
-
