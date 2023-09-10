e243af1648c9ed8eeb11d378ab4f31c4c45e2f52
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-10 23:13:12 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,15 +9,15 @@
 
 int main(void)
 {
-	int ch= sizeof(char);
+	char ch= sizeof(ch);
 	printf("Size of a char: %d byte(s)\n", ch);
-	int n= sizeof(int);
+	int n= sizeof(n);
         printf("Size of a int: %d byte(s)\n", n);
-	int lint= sizeof(int);
+	int lint= sizeof(lint);
         printf("Size of a long int: %ld byte(s)\n", lint);
-	int llint= sizeof(int);
+	int llint= sizeof(llint);
         printf("Size of a long long int: %lld byte(s)\n", llint);
-	int f= sizeof(float);
+	int f= sizeof(f);
         printf("Size of a float: %f byte(s)\n", f);
 	
 	return (0);
