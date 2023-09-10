ffe1e2b23e51f9aea4e998cfc7cf1122bdaa7390
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-10 23:15:45 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
         printf("Size of a int: %d byte(s)\n", n);
 	int lint= sizeof(lint);
         printf("Size of a long int: %ld byte(s)\n", lint);
-	int llint= sizeof(llint);
+	long llint= sizeof(llint);
         printf("Size of a long long int: %lld byte(s)\n", llint);
-	int f= sizeof(f);
+	float f= sizeof(f);
         printf("Size of a float: %f byte(s)\n", f);
 	
 	return (0);
