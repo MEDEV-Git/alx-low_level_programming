e6b17c31eb576497a9de12f483a65dc9f16bb124
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-10 22:15:43 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,11 @@
 # include <stdio.h>
+/**
+* main - entry point
+*
+* Description: print with printf
+*
+* Return: 0 (Success)
+*/
 int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
