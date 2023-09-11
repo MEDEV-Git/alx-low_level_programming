ba9de5099c51b537a692eae7a5f9ef04ad8a589e
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-12 00:32:59 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
  * main - Entry point
@@ -14,37 +13,29 @@
 int main(void)
 {
     int n;
-    char nStr[30];
-    int length;
-    int index;
-    int absoluteN;
-
+    
     srand(time(0));
     n = rand() - RAND_MAX / 2;
 
-    /* Calculate the absolute value of n */
-    absoluteN = abs(n);
-
-    /* Convert absoluteN to a string */
+    int absoluteN = abs(n);
+    char nStr[30];
     sprintf(nStr, "%d", absoluteN);
 
-    /* Calculate the length of the string */
-    length = strlen(nStr);
+    char lastDigit = nStr[strlen(nStr) - 1];
 
-    /* Calculate the index of the last character */
-    index = length - 1;
+    printf("Last digit of %d is %c", n, n < 0 ? '-' : ' ');
 
-    if (nStr[index] > '5')
+    if (lastDigit > '5')
     {
-        printf("Last digit of %d is %c and is greater than 5\n", n, nStr[index]);
+        printf("%c and is greater than 5\n", lastDigit);
     }
-    else if (nStr[index] < '6' && nStr[index] != '0')
+    else if (lastDigit < '6' && lastDigit != '0')
     {
-        printf("Last digit of %d is %c and is less than 6 and not 0\n", n, nStr[index]);
+        printf("%c and is less than 6 and not 0\n", lastDigit);
     }
-    else if (nStr[index] == '0')
+    else if (lastDigit == '0')
     {
-        printf("Last digit of %d is %c and is 0\n", n, nStr[index]);
+        printf("%c and is 0\n", lastDigit);
     }
 
     return (0);
