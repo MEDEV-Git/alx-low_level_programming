6d360cfc24c3c4cad18960e02682a4e021d98e4d
Mohammed Goubi <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-11 16:10:51 -0700

    Create 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,22 +1,42 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - entry point
  *
- * Description: The last digit
+ * Description: find last digit
  *
  * Return: 0 (Success)
- *
  */
 
 int main(void)
 {
-	int n;
+    int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    
+    char nStr[30];
+    sprintf(nStr, "%d", n);
+    
+    int length = strlen(nStr);
+    int index = length - 1;
+
+    if (nStr[index] > '5')
+    {
+        printf("Last digit of %d is %c and is greater than 5\n", n, nStr[index]);
+    }
+    else if (nStr[index] < '6' && nStr[index] != '0')
+    {
+        printf("Last digit of %d is %c and is less than 6 and not 0\n", n, nStr[index]);
+    }
+    else if (nStr[index] == '0')
+    {
+        printf("Last digit of %d is %c and is 0\n", n, nStr[index]);
+    }
+
+    return (0);
 }
+
