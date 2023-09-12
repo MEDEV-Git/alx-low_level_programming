af8a415281a14d333a733848bacf25584e40cf4f
Mohammed Goubi <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-11 18:04:50 -0700

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,13 +10,12 @@
 
 int main(void)
 {
-    char hexDigits[] = "0123456789abcdef";
-    int i;
-    
-    for (i = 0; i < 16; i++)
-    {
-        putchar(hexDigits[i]);
-    }
-    putchar('\n');
-    return (0);
+	char hexDigits[] = "0123456789abcdef";
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexDigits[i]);
+	}
+	putchar('\n');
+	return (0);
 }
