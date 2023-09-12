e0b52bf6c07421bab52c7cc85109b31d0e96366f
Mohammed Goubi <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-11 17:18:12 -0700

    Create 6- print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int numberz=0;
+
+	while (numberz <= 9)
+	{
+		printf("%d", numberz);
+		numberz+=1;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
