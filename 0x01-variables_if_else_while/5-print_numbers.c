24dce26d58e85b5a58568dc203d8cda95248d92b
Mohammed Goubi <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-11 17:05:28 -0700

    Create 7 see in mirror

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Description: print number base-10
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("echo {0..9}");
+	return (0);
+}
