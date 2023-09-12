be39b427953ec1aef6c67e83602436b6851b9f42
Mohammed Goubi <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-11 17:57:13 -0700

    Create 8 base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Description: hexa digit
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+    char hexDigits[] = "0123456789abcdef"; // Hexadecimal digits in lowercase
+    int i;
+
+    for (i = 0; i < 16; i++)
+    {
+        putchar(hexDigits[i]);
+    }
+
+    putchar('\n');
+    return (0);
+}
+
