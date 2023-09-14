2eba2123fbafcd006a1552e17788b9a31e759e2a
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-14 23:44:10 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -17,7 +17,7 @@ int _islower(int c)
 	r = _islower('H');
 	_putchar(r + '0');
 	r = _islower('o');
-	_putchar(c + '0');
+	_putchar(r + '0');
 	r = _islower(108);
 	_putchar(r + '0');
 	_putchar('\n');
