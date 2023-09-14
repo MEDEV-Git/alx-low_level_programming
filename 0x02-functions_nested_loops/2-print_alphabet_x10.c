dc132f9b8f196dc40195fd82b3662202259a41ee
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-14 22:31:19 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,9 +15,9 @@ int print_alphabet_x10(void)
 	char c;
 	int j;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (j =0; j <= 10; j++)
 	{
-		for (j = 0; j <= 10; j++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
