c177072474abbaa3ad986729863618510ba64292
Mohammed Goubi <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-14 10:12:50 -0700

    Update file 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,25 +2,28 @@
 #include "main.h"
 
 /**
+ * main - entry point
+ *
  * _putchar - writes the character c to stdout
  * @c: The character to print
+ *
+ *Description: write _putchar
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int main(void) 
+int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
 
