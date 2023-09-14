7b1b73e1986b649328c6d51eb6e9462d2d6613a7
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 00:10:12 +0100

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+* _isalpha - function to check lowercase
+*
+* @c: The character to check
+*
+* Description: check if a charachter is alphabetic
+*
+* Return: 1 if c is alphabetic, 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
