4e2a0433ab7aebb472a57a38787b1bd9dc99a1bf
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 00:01:13 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
 #include "main.h"
 
 /**
- * _islower - function to check lowercase
- *
- * Description: check if a charachter is lowercase
- *
- * Return: 1 Succes
- */
+* _islower - function to check lowercase
+*
+* Description: check if a charachter is lowercase
+*
+* Return: 1 Succes
+*/
 
 int _islower(int c)
 {
