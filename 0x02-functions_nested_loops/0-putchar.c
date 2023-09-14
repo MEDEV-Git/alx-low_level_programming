c5e19d1a9855fbc9cfe077e0ac1c960597882a3d
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-14 18:15:11 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,8 @@
  * main - entry point
  *
  * _putchar - writes the character c to stdout
- * @c: The character to print
  *
- *Description: write _putchar
+ * Description: write _putchar @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
