08dac8d923bac8ac290aa2a8182c65688e776c5b
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 03:02:41 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* print_most_numbers - print numbers
+* print_most_numbers - print numbers from 0 - 9 except 2 and 4
 *
 * Description: print numbers from 0 to 9 except 2 and 4
 *
