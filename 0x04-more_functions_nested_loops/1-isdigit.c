ee530461c4321fccb00a60a11f87e0a8f0394a66
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 01:22:49 +0100

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+* _isdigit - function to check for digit
+*
+* @c: The digit to check
+*
+* Description: check if a charachter is digit
+*
+* Return: 1 if c is digit, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' || c <= '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
