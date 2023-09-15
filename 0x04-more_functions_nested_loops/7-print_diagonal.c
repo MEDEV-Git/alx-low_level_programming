327c086ceafd63aa02c3f0da0686e737724fe80c
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 03:59:09 +0100

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_diagonal - print _ depending on input n
+*
+* @n: input
+*
+* Description: print \ diagonal
+*
+* Return: 0 Success;
+*/
+
+void print_diagonal(int n)
+{
+	int lenchr;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (lenchr = 1; lenchr <= n; lenchr++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+	
+}
