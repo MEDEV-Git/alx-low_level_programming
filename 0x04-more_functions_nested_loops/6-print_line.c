a6aa00a9f905ec9fcdbc44067f35379729fa5e5c
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 03:46:30 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -16,11 +16,12 @@ void print_line(int n)
   int lenchr;
   if (n <= 0)
   {
-    _putchar(n);
+    _putchar('\n');
   }
   else 
     for (lenchr = 1; lenchr <= n; lenchr++)
       {
         _putchar('_');
+        _putchar('\n');
       }
 }
