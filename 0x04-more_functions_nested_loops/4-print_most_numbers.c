a44af100b3cdf813a398769590672a51329fcb4c
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 02:54:33 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* print_most_numbers - print numbers
+*
+* Description: print numbers from 0 to 9 except 2 and 4
+*
+* Return: 0 (Success)
+*
+*/
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; (n <= 57) && (n != 2 && n != 4); n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
 
