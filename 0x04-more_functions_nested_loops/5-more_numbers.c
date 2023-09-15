d190c1da40d63397006dc3ea43c944b368b8ab8b
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 03:14:40 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,28 @@
+#include <unistd.h>
+#include "main.h"
 
+/**
+* more_numbers - function to print numbers from 0 to 14 x10times
+*
+* Description: print from 0 to 14 x10times
+*
+* Return: 0 (Success)
+*/
+
+void more_numbers(void)
+{
+	int n;
+	int j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		n = 0;
+		while (n <= 14)
+		{
+			_putchar(n);
+			n++;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
