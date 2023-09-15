8469c5bb2143a69220a76b1179ef6fd90fcd8391
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 03:43:57 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,26 @@
+#include <unistd.h>
+#include "main.h"
 
+/**
+* print_line - print _ depending on input n
+*
+* @n: input
+*
+* Description: print _ lign
+*
+* Return: 0 Success;
+*/
+
+void print_line(int n)
+{
+  int lenchr;
+  if (n <= 0)
+  {
+    _putchar(n);
+  }
+  else 
+    for (lenchr = 1; lenchr <= n; lenchr++)
+      {
+        _putchar('_');
+      }
+}
