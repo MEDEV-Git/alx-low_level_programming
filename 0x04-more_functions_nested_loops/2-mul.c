a4e298f0edb191a752e005f4ec275559ccb9c45c
MEDEV <79004734+MEDEV-Git@users.noreply.github.com>
2023-09-15 02:18:59 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,12 @@
 #include "main.h"
 
 /**
+*
 * mul - function to multiply two integer
 *
 * Description: multiply two integer
 *
-*Return: 0 Success
+* Return: 0 Success
 */
 
 int mul(int a, int b)
